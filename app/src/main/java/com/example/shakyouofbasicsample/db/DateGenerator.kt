8133960814a040b9eabecab53b417870e931287f
youhei <oonuma617@gmail.com>
2019-05-15 06:23:44 +0900

    DateGeneratorを追加

---
+++ app/src/main/java/com/example/shakyouofbasicsample/db/DateGenerator.kt
@@ -0,0 +1,21 @@
+package com.example.shakyouofbasicsample.db
+
+
+class DateGenerator {
+
+    companion object {
+
+        private val FIRST = arrayOf(
+            "Special edition", "New", "Cheap", "Quality", "Used"
+        )
+
+        private val SECOND = arrayOf(
+            "Three-headed Monkey", "Rubber Chicken", "Pint of Grog", "Monocle"
+        )
+
+        private val DESCRIPTION = arrayOf(
+            "is finally here", "is recommended by Stan S. Stanman",
+            "is the best sold product on Mêlée Island", "is \uD83D\uDCAF", "is ❤️", "is fine"
+        )
+    }
+}
